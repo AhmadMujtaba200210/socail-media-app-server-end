a5f0a564acd9299f8574e3fffd6ac5d9889add68
Ahmad Mujtaba <cadeta6@gmail.com>
2023-01-08 19:21:55 +0500

    feat: logging in added

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,9 +6,9 @@ import User from "../models/User.js";
 // WRITING AUTHENTICATION API
 // we have created it async because we are accessing the database at real time simultaneously, req: anything asked from frontend is called as request,
 // res: anything in return of that request,response came from backend ,is called response
-export const register=async(req,res)=>{  
+export const register = async (req, res) => {
     try {
-        const{
+        const {
             firstName,
             lastName,
             email,
@@ -16,13 +16,13 @@ export const register=async(req,res)=>{
             picturePath,
             friends,
             location,
-            occupation
-        }=req.body; // from the frontend we will receive the body parameter which is having all the data mentioned in the const
+            occupation,
+        } = req.body;// from the frontend we will receive the body parameter which is having all the data mentioned in the const
 
-        const salt= await bcrypt.genSalt(); // using bcrypt password encoder
-        const passwordHash=await bcrypt.hash(password,salt); // creating a password hash by using the password parameter from the req body
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);// creating a password hash by using the password parameter from the req body
         // we are going to encrypt the password and save it and once user asked the password we wil decode it again and return as jason web token
-        const newUser=new User({
+        const newUser = new User({
             firstName,
             lastName,
             email,
@@ -31,13 +31,31 @@ export const register=async(req,res)=>{
             friends,
             location,
             occupation,
-            viewedProfile: Math.floor(Math.random()*10000),
-            impressions: Math.floor(Math.random()*10000)
-        })
-        const savedUser= await newUser.save();
+            viewedProfile: Math.floor(Math.random() * 10000),
+            impressions: Math.floor(Math.random() * 10000),
+        });
+        const savedUser = await newUser.save();
         // once the new user is saved we will send a status code of 201 and also the new saved user in response body
-        res.status(201).json(savedUser); 
-    } catch (error) {
-        res.status(500).json({error: error.message});
+        res.status(201).json(savedUser);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
-}
+};
+
+/* LOGGING IN */
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+        if (!user) return res.status(400).json({ msg: "User does not exist. " });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
+
+        const token = jwt.sign({ id: user._id }, "secret");
+        delete user.password;
+        res.status(200).json({ token, user });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
--- models/User.js
+++ models/User.js
@@ -1,50 +1,45 @@
 import mongoose from "mongoose";
 
-//When ever we going to create USer model first it will create user schema and later it passes it to user model.
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 50,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 50,
+        },
+        email: {
+            type: String,
+            required: true,
+            max: 50,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 5,
+        },
+        picturePath: {
+            type: String,
+            default: "",
+        },
+        friends: {
+            type: Array,
+            default: [],
+        },
+        location: String,
+        occupation: String,
+        viewedProfile: Number,
+        impressions: Number,
+    },
+    { timestamps: true }
+);
 
-
-// here we are going to create an schema of user model with properties and validations
-const UserSchema=new mongoose.Schema({
-    fistName:{
-        type:String,
-        required:true,
-        min:2,
-        max:50,
-    },
-    lastName:{
-        type:String,
-        required:true,
-        min:2,
-        max:50,
-    },
-    email:{
-        type:String,
-        required:true,
-        max:50,
-        unique: true
-    },
-    password:{
-        type:String,
-        required:true,
-        min:5,
-    },
-    picturePath:{
-        type:String,
-        default:"",
-    },
-    friends:{
-        type: Array,
-        default: []
-    },
-    location: String,
-    occupation: String,
-    viewedProfile: Number,
-    impressions: Number,
-
-},
-{timestamps:true}); // whenever a user is registered the time will also store in the database
-
-//then we pass the user schema to the mongoose model to create a user object model
-const User=mongoose.model("User",UserSchema);
-
+const User = mongoose.model("User", UserSchema);
 export default User;
